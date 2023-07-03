00b5c45e1364624b9e064be8056fee989d6b2e2d
gulamovadbuvakhob <gulamovadbuvakhob@gmail.com>
2023-07-03 11:33:24 +0500

    backend done

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import logging
-from buttons import but_start
+from buttons import but_start,but_start1,but_start2
 import aiogram
 
 API_TOKEN = '6077407519:AAEsWutj8nqQ5U9BH6ZHuBAECd1bZB8WAhQ'
@@ -28,9 +28,41 @@ async def echo(message: aiogram.types.Message):
 async def man(message: aiogram.types.Message):
     await message.reply_location(40.3911725263081, 71.78637436836638)
 
-@dp.message_handler(text_contains=["Backend"])
-async def man(message: aiogram.types.Message):
-    await message.answer(reply_markup=)
+@dp.message_handler(text_contains=["Backend darslari 👨🏻‍💻"])
+async def darslar(message: aiogram.types.Message):
+    await message.answer("BACKEND",reply_markup=but_start1)
+
+@dp.message_handler(text_contains=["About backend"])
+async def about(message: aiogram.types.Message):
+    await message.answer("""In ENGLISH🇬🇧:The back end refers to parts of a computer application or a program's code that allow it to operate and that cannot be accessed by a user. Most data and operating syntax are stored and accessed in the back end of a computer system. Typically the code is comprised of one or more programming languages. 
+    На РУССКОМ🇷🇺:Серверная часть относится к частям компьютерного приложения или кода программы, которые позволяют ему работать и к которым пользователь не может получить доступ . Большинство данных и рабочего синтаксиса хранятся и доступны в задней части компьютерной системы. Обычно код состоит из одного или нескольких языков программирования.""")
+
+@dp.message_handler(text_contains=["Orqaga"])
+async def back(message:aiogram.types.Message):
+    await message.reply("Orqaga",reply_markup=but_start)
+
+@dp.message_handler(text_contains=["qoshilish"])
+async def ariza(message:aiogram.types.Message):
+    await message.reply("Darslarga qoshilishni hohlasangiz pastdagi raqamga murojat qilishingiz mumkin. A dan Zed gacha birga!!")
+    await message.answer_contact("+998945327744","Zed it","academy")
+
+@dp.message_handler(text_contains=["chilar"])
+async def oquvchi(message:aiogram.types.Message):
+    await message.answer("Hozirda backend oqiyatgan oquvchilar soni 4 ta.")
+
+@dp.message_handler(text_contains=["Uztozlar"])
+async def ustoz(message:aiogram.types.Message):
+    await message.answer("Hozirda backend darslarini malakali ustoz Azamjon Usmonaliyev otib boryaptilar.")
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     aiogram.executor.start_polling(dp, skip_updates=True)
--- buttons.py
+++ buttons.py
@@ -1,7 +1,7 @@
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton
 
-but_back = KeyboardButton("Backend darslari")
-but_front = KeyboardButton("Frontend darslari")
+but_back = KeyboardButton("Backend darslari 👨🏻‍💻")
+but_front = KeyboardButton("Frontend darslari 👨‍💻")
 but_tel = KeyboardButton("Qong'iroq uchun☎️")
 but_man = KeyboardButton("Manzilimiz📍")
 but_kan = KeyboardButton("Kanalimiz🔗")
@@ -12,11 +12,13 @@ but_start = ReplyKeyboardMarkup().add(but_back, but_front, but_man, but_tel, but
 but_oqit = KeyboardButton("Uztozlar")
 but_oquv = KeyboardButton("Backendchilar")
 but_ariza = KeyboardButton("Darslarga qoshilish")
-but_mal = KeyboardButton("About 'Backend'")
-
-but_backend
+but_mal = KeyboardButton("About backend")
+but_orqa = KeyboardButton("Orqaga")
+but_start1 = ReplyKeyboardMarkup().add(but_oqit,but_oquv,but_ariza,but_mal,but_orqa)
 
 but_oqit1 =KeyboardButton("Ustozlar")
 but_oquv1 = KeyboardButton("Frontendchilar")
 but_ariza1 = KeyboardButton("Darslarga qoshilish")
 but_mal1 = KeyboardButton("About 'Frontend'")
+
+but_start2 = ReplyKeyboardMarkup().add(but_oqit1,but_oquv1,but_ariza1,but_mal1,but_orqa)
